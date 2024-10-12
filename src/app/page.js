dfc2de947251ec5441bfd13e988acb4d364f01f7
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-10-12 17:55:37 +0500

    resolving deploy  issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import "./globals.css";
 import { addTodo } from "./actions/todos";
+import Navbar from "@/components/Navbar";
 export default async function Home() {
   let todosApi = await fetch("http://localhost:3000/api/todos");
   todosApi = await todosApi.json();
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <h1 className="text-center text-3xl">Hamza</h1>
       <form action={addTodo} className="flex justify-center items-center my-4 gap-2">
         <input type="text" placeholder="Enter Your Todo" className="border border-black px-2 py-3 w-1/4" name="todo" />
